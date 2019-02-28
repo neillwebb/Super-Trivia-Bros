e5069c3dcf32a9d970a8e2325bdc79a8f7ed30cb
gbaek3 <g.baek24@gmail.com>
2019-02-28 02:14:37 -0500

    seed, index, package, question model

---
+++ seeds/seed-db.js
@@ -0,0 +1,20 @@
+const db = require("../models");
+const questions = [
+  {
+    category: "Entertainment: Books",
+    difficulty: "medium",
+    question: "Which of the following authors was not born in England? ",
+    correct_answer: "Arthur Conan Doyle",
+    incorrect_answers: "Graham Greene"
+  }
+];
+
+db.sequelize.sync({ force: true }).then(function() {
+  db.Question.bulkCreate(questions)
+    .then(function(rows) {
+      console.log("Data successfully added!");
+    })
+    .catch(function(error) {
+      console.log("Error", error);
+    });
+});
