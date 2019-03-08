ee5fba55a3b96a184099adee36f90a14f5f26f80
Neill Webb <neill.webb@gmail.com>
2019-03-08 13:52:31 -0500

    added some html elements for leaderboard and personal high score

--- client/src/components/LeaderBoard.js
+++ client/src/components/LeaderBoard.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react";
 
-const LeaderBoard = (props) => (
-    <div>
-   <p>
-     This is the Leader Board! Enter Your Initials Here To Go Down In History!
-   </p>
- </div>
+const LeaderBoard = props => (
+  <div>
+    <h1>Congratulations! You've made the Leader Board!</h1>
+    <p>
+      <h2>LeaderBoard</h2>
+      <ol>
+        <li class= "leaderboardPosition" id= "leader1"></li>
+        <li class= "leaderboardPosition" id= "leader2"></li>
+        <li class= "leaderboardPosition" id= "leader3"></li>
+        <li class= "leaderboardPosition" id= "leader4"></li>
+        <li class= "leaderboardPosition" id= "leader5"></li>
+        <li class= "leaderboardPosition" id= "leader6"></li>
+        <li class= "leaderboardPosition" id= "leader7"></li>
+        <li class= "leaderboardPosition" id= "leader8"></li>
+        <li class= "leaderboardPosition" id= "leader9"></li>
+        <li class= "leaderboardPosition" id= "leader10"></li>
+        
+      </ol>
+    </p>
+  </div>
 );
 
 export default LeaderBoard;
--- client/src/components/PersonalHighScore.js
+++ client/src/components/PersonalHighScore.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
 
-const PersonalHighScore = (props) => (
-    <div>
-   <p>
-     This is Where You Can See Your Personal High Score!
-   </p>
- </div>
-);
-
+const PersonalHighScore = props => (
+  <div>
+    <h1> Personal High Scores! </h1>
+    <p>
+      <ul>
+        <li class="highScoreCategory" id="highScoreBooks">Books:</li>
+        <li class="highScoreCategory" id="highScoreMovies">Movies:</li>
+        <li class="highScoreCategory" id="highScoreSports">Sports:</li>
+        <li class="highScoreCategory" id="highScoreTelevision">Television:</li>
+        <li class="highScoreCategory" id="highScoreVideoGames">Video Games:</li>
+        <li class="highScoreCategory" id="highScoreMusic">Music:</li>
+        <li class="highScoreCategory" id="highScoreScienceNature">Science and Nature:</li>
+        <li class="highScoreCategory" id="highScoreGeneralKnowledge">General Knowledge:</li>
+        <li class="highScoreCategory" id="highScoreHistory">History:</li>
+        <li class="highScoreCategory" id="highScoreGeography">Geography:</li>
+        <li class="highScoreCategory" id="highScoreCelebrities">Celebrities:</li>
+        <li class="highScoreCategory" id="highScoreAnimals">Animals:</li>
+      </ul>
+    </p>
+  </div>
+)
 export default PersonalHighScore;
