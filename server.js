ad001bb4083dfa5a21efca1904ab0662ed68ecab
Juzcategui <gotdakey@gmail.com>
2019-03-08 10:46:58 -0500

    file structure setup

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 
 const db = require("./models");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "./public")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
 require("./routing/api-routes.js")(app);
 require("./routing/html-routes.js")(app);
@@ -18,6 +21,6 @@ const MONGODB_URI =
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
-app.listen(PORT, function () {
+app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
