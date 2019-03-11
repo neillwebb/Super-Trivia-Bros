83d9315df8fe8609f5500aefcdea1e1576304751
gbaek3 <g.baek24@gmail.com>
2019-03-11 14:23:26 -0400

    more react stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,77 +1,22 @@
-import React, { Component } from "react";
-import Form from '../src/components/Form';
+import React from "react";
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
+import Login from '../src/components/Login';
+import Trivia from '../src/components/Trivia';
 import GameWindow from '../src/components/GameWindow';
 import "./App.css";
-import * as $ from 'axios';
+import image from "../src/image.png"
 
-class App extends Component {
-  state = {
-    username: '',
-    password: '',
-    reguser: '',
-    regpw: '',
-    category: ["Books", "Movies", "Sports", "Television", 'Celebrities', "Animals", "Geography", "History", "VideoGames", "ScienceAndNature", "GeneralKnowledge", "Music"],
-    allScores: [],
-    score: 0,
-    isLoggedin: false,
-    easterEgg: false
-  }
-
-  handleChange = (event) => {
-    this.setState({
-      [event.target.name]: event.target.value.toLowerCase()
-    })
-  }
-
-  categoryClick = (event) => {
-    event.preventDefault();
-    this.setState({
-      category: event.target.name
-    })
-  }
-
-  handleRegister = (event) => {
-    event.preventDefault();
-    $.post('/api/user', { username: this.state.reguser, password: this.state.regpw })
-      .then((data) => {
-        console.log(data)
-      }).catch(function (err) {
-        alert("Invalid username or password (Username might be taken)")
-      })
-  }
-
-  handleLogin = (event) => {
-    event.preventDefault();
-    $.post('/api/session', { username: this.state.username, password: this.state.password })
-      .then((data) => {
-        console.log(data)
-        this.setState({
-          isLoggedin: true,
-          username: this.state.username,
-          password: this.state.password,
-          allScores: data.data.scores
-        })
-      }).catch(function (err) {
-        alert("Username or password is incorrect")
-      })
-
-  }
-
-  render() {
-    return (
-      <div>
-        {this.state.isLoggedin === false ?
-          <Form onChangeHandler={this.handleChange}
-            regHandler={this.handleRegister}
-            loginHandler={this.handleLogin} /> :
-          <GameWindow categories={this.state.category} />
-
-
-        }
-
-      </div>
-    );
-  }
-}
+const App = () => (
+  <BrowserRouter>
+    <div>
+      <header>
+        <img src={image} className="background" alt="background" />
+      </header>
+      <Route exact path='/' component={Login} />
+      <Route exact path='/gamewindow' component={GameWindow} />
+      <Route exact path='/trivia' component={Trivia} />
+    </div>
+  </BrowserRouter >
+)
 
 export default App;
--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,19 +1,45 @@
 import React from "react";
 
-
-const Form = (props) => (
-    <form>
+const Form = props => (
+    <form className="login">
         <h3>Register</h3>
-        <input name="reguser" placeholder="Username" onChange={props.onChangeHandler} />
-        <input name="regpw" type="password" placeholder="Password" onChange={props.onChangeHandler} />
-        <button onClick={props.regHandler}> Register </button>
+        <input
+            name="reguser"
+            placeholder="Username"
+            className="inputField"
+            onChange={props.onChangeHandler}
+        />
+        <input
+            name="regpw"
+            type="password"
+            placeholder="Password"
+            className="inputField"
+            onChange={props.onChangeHandler}
+        />
+        <button className="loginButton" onClick={props.regHandler}>
+            {" "}
+            Register{" "}
+        </button>
 
         <h3>Login</h3>
-        <input name="username" placeholder="Username" onChange={props.onChangeHandler} />
-        <input name="password" type="password" placeholder="Password" onChange={props.onChangeHandler} />
-        <button onClick={props.loginHandler}> Login </button>
+        <input
+            name="username"
+            placeholder="Username"
+            className="inputField"
+            onChange={props.onChangeHandler}
+        />
+        <input
+            name="password"
+            type="password"
+            placeholder="Password"
+            className="inputField"
+            onChange={props.onChangeHandler}
+        />
+        <button className="loginButton" onClick={props.loginHandler}>
+            {" "}
+            Login{" "}
+        </button>
     </form>
+);
 
-)
-
-export default Form
+export default Form;
