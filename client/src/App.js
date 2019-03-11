bfb88a860f89374732538f642de47e935cb81065
Paige <mpaigemartin@gmail.com>
2019-03-11 13:33:24 -0400

    css styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,74 +1,96 @@
 import React, { Component } from "react";
-import Form from '../src/components/Form';
-import GameWindow from '../src/components/GameWindow';
+import Form from "../src/components/Form";
+import GameWindow from "../src/components/GameWindow";
 import "./App.css";
-import * as $ from 'axios';
+import * as $ from "axios";
+import image from "../src/image.png";
 
 class App extends Component {
   state = {
-    username: '',
-    password: '',
-    reguser: '',
-    regpw: '',
-    category: ["Books", "Movies", "Sports", "Television", 'Celebrities', "Animals", "Geography", "History", "VideoGames", "ScienceAndNature", "GeneralKnowledge", "Music"],
+    username: "",
+    password: "",
+    reguser: "",
+    regpw: "",
+    category: [
+      "Books",
+      "Movies",
+      "Sports",
+      "Television",
+      "Celebrities",
+      "Animals",
+      "Geography",
+      "History",
+      "VideoGames",
+      "ScienceAndNature",
+      "GeneralKnowledge",
+      "Music"
+    ],
     allScores: [],
     score: 0,
     isLoggedin: false,
     easterEgg: false
-  }
+  };
 
-  handleChange = (event) => {
+  handleChange = event => {
     this.setState({
       [event.target.name]: event.target.value.toLowerCase()
-    })
-  }
+    });
+  };
 
-  categoryClick = (event) => {
+  categoryClick = event => {
     event.preventDefault();
     this.setState({
       category: event.target.name
+    });
+  };
+
+  handleRegister = event => {
+    event.preventDefault();
+    $.post("/api/user", {
+      username: this.state.reguser,
+      password: this.state.regpw
     })
-  }
-
-  handleRegister = (event) => {
-    event.preventDefault();
-    $.post('/api/user', { username: this.state.reguser, password: this.state.regpw })
-      .then((data) => {
-        console.log(data)
-      }).catch(function (err) {
-        alert("Invalid username or password (Username might be taken)")
+      .then(data => {
+        console.log(data);
       })
-  }
+      .catch(function(err) {
+        alert("Invalid username or password (Username might be taken)");
+      });
+  };
 
-  handleLogin = (event) => {
+  handleLogin = event => {
     event.preventDefault();
-    $.post('/api/session', { username: this.state.username, password: this.state.password })
-      .then((data) => {
-        console.log(data)
+    $.post("/api/session", {
+      username: this.state.username,
+      password: this.state.password
+    })
+      .then(data => {
+        console.log(data);
         this.setState({
           isLoggedin: true,
           username: this.state.username,
           password: this.state.password,
           allScores: data.data.scores
-        })
-      }).catch(function (err) {
-        alert("Username or password is incorrect")
+        });
       })
-
-  }
+      .catch(function(err) {
+        alert("Username or password is incorrect");
+      });
+  };
 
   render() {
     return (
       <div>
-        {this.state.isLoggedin === false ?
-          <Form onChangeHandler={this.handleChange}
+        <img src={image} className="background" alt="background" />
+        {this.state.isLoggedin === false ? (
+          <Form
+            onChangeHandler={this.handleChange}
             regHandler={this.handleRegister}
-            loginHandler={this.handleLogin} /> :
+            loginHandler={this.handleLogin}
+          />
+        ) : (
           <GameWindow categories={this.state.category} />
-
-
-        }
-
+        )}
       </div>
     );
   }
