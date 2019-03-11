54f69c41edb576361058f0b911280e22fbfe74ad
gbaek3 <g.baek24@gmail.com>
2019-03-11 19:55:42 -0400

    trivia page

--- client/src/components/GameWindow.js
+++ client/src/components/GameWindow.js
@@ -1,48 +1,19 @@
 import React from 'react';
 import Category from '../components/Category';
-import { Link } from 'react-router-dom';
-import Trivia from '../components/Trivia';
+import Login from '../components/Login';
 import '../App.css'
 import * as $ from 'axios';
 
 class GameWindow extends React.Component {
     state = {
         categories: ["Books", "Movies", "Sports", "Television", 'Celebrities', "Animals", "Geography", "History", "Video Games", "Science And Nature", "General Knowledge", "Music"],
-        selectedCategory: '',
-        difficulty: '',
         scoreList: this.props.allScores,
-        difficultySelected: false,
-        categorySelected: false,
         score: 0
-    }
-    gameStart = (event) => {
-        event.preventDefault();
-        this.setState({
-            categorySelected: true,
-            selectedCategory: event.target.name
-        })
-    }
-
-
-    renderTrivia() {
-        console.log("hello");
-        return <Trivia diff={this.state.difficulty} />
-
-    }
-
-    pullTrivia = (event) => {
-        event.preventDefault();
-        $.get(`/api/question/${this.state.selectedCategory}`).then((data) => {
-            console.log(data)
-            { this.renderTrivia() };
-
-        })
-
     }
 
     getCategories() {
         return this.state.categories.map((data, i) => (
-            <Category key={i} showGame={this.gameStart}
+            <Category key={i}
                 name={data} />
         ))
     }
@@ -51,19 +22,11 @@ class GameWindow extends React.Component {
         return (
             <div>
                 {this.getCategories()}
-                {this.state.categorySelected === false ?
-                    <header>
-                        <h1>Super Trivia Bros!</h1>
-                        <h2>Choose a category!</h2>
-                    </header>
-                    :
-                    <div>
-                        <h1>Super Trivia Bros!</h1>
-                        <h1> {this.state.category}</h1>
+                <header>
+                    <h1>Super Trivia Bros!</h1>
+                    <h2>Choose a category!</h2>
+                </header>
 
-
-                    </div>
-                }
             </div>
         )
     }
--- client/src/components/Trivia.js
+++ client/src/components/Trivia.js
@@ -1,36 +1,100 @@
 import React from 'react';
-
+import Question from '../components/Question';
+import AnswerOption from '../components/AnswerOption';
 import * as $ from 'axios';
 
 class Trivia extends React.Component {
     state = {
         category: this.props.location.hash.substring(1),
         difficulty: '',
+        questionList: [],
+        answerList: [],
+        count: 0,
+        answerCount: 0,
         scoreList: this.props.allScores,
         difficultySelected: false,
+        gameFinished: false,
         score: 0
     }
 
     diffcultyClick = (event) => {
         event.preventDefault();
-        this.setState({
-            difficultySelected: true,
-            difficulty: event.target.name
-        })
-
+        let temp = event.target.name;
+        // this.setState({
+        //     questionList: [],
+        //     answerList: []
+        // })
         $.get(`/api/question/${this.state.category}`).then((data) => {
-            console.log(data)
+            console.log(data);
+            const tempArray = [];
+            for (let i = 0; i < 10; i++) {
+                tempArray[i] = (data.data[i].incorrect_answers).concat(data.data[i].correct_answer)
+                this.setState({
+                    difficulty: temp,
+                    difficultySelected: true,
+                    questionList: this.state.questionList.concat(data.data[i].question),
+                })
+            }
+            this.setState({
+                answerList: tempArray
+            })
+            console.log(tempArray);
+
         })
+    };
+
+    getQuestions() {
+        return <Question name={this.state.questionList[this.state.count]} />
+
     }
+    getAnswers() {
+        return <AnswerOption name={this.state.answerList[this.state.count]} />
+
+        // return (this.state.answerList[this.state.count]).map((data, i) => (
+        //     <AnswerOption key={i}
+        //         name={data} />
+        // ))
+    }
+
+    nextQuestion = (event) => {
+        event.preventDefault();
+        if (this.state.count < 9) {
+            this.setState({
+                count: this.state.count + 1,
+                answerCount: this.state.answerCount + 1
+            })
+        }
+        else {
+            this.setState({
+                gameFinished: true
+            })
+        }
+    }
+
 
     render() {
         return (
             < div >
-                <h1>{this.props.location.hash.substring(1)} Trivia </h1>
-                <button name="Easy" onClick={this.diffcultyClick}>Easy</button>
-                <button name="Medium" onClick={this.diffcultyClick}>Medium</button>
-                <button name="Hard" onClick={this.diffcultyClick}>Hard</button>
-            </div >
+                {
+                    this.state.difficultySelected === false ?
+                        <div>
+                            <h1>{this.props.location.hash.substring(1)} Trivia </h1>
+                            <button name="Easy" onClick={this.diffcultyClick}>Easy</button>
+                            <button name="Medium" onClick={this.diffcultyClick}>Medium</button>
+                            <button name="Hard" onClick={this.diffcultyClick}>Hard</button>
+                        </div > :
+
+                        this.state.gameFinished === false ?
+                            <div>
+                                {this.getQuestions()}
+                                {this.getAnswers()}
+                                <button name="next" onClick={this.nextQuestion}>Next</button>
+                            </div> :
+                            <div>Your score was: </div>
+
+
+                }
+            </div>
         )
     }
 }
